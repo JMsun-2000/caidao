22bca21f15966e9e59335c117cd3eee2e8a1d2b6
Sunjiaming <jmsun_2000@hotmail.com>
2015-10-10 23:52:30 +0800

    add filter for store

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,7 +2,13 @@ class StoreController < ApplicationController
   skip_before_filter :authorize
 
   def index
-    @products = Product.all
+    filter = params[:product_filter]
+    if filter
+      @products = Product.where(:product_type => filter).to_a
+    else
+      @products = Product.all
+    end
+    @product_filters = Product.all.uniq.pluck(:product_type)
     @cart = current_cart
   end
 end
