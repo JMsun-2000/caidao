8b70295b9d0a0cfa21ad4494b31a9e8ead4010bf
Sunjiaming <jmsun_2000@hotmail.com>
2015-11-02 22:48:03 +0800

    add style for filter

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,9 +2,16 @@ class StoreController < ApplicationController
   skip_before_filter :authorize
 
   def index
-    filter = params[:product_filter]
-    if filter
-      @products = Product.where(:product_type => filter).to_a
+    @filter = params[:product_filter]
+    if @filter
+      filter_sub = params[:product_sub_filter]
+      if filter_sub
+        @products = Product.where(:product_type => @filter, :sub_type => filter_sub).to_a
+      else
+        @products = Product.where(:product_type => @filter).to_a
+      end
+
+      @sub_filters = Product.where(:product_type => @filter).uniq.pluck(:sub_type)
     else
       @products = Product.all
     end
